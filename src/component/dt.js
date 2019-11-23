eceec80b506073eafae1e52247ce10838d124659
unknown <dadang8396@gmail.com>
2019-11-23 17:17:30 +0700

    first-commit

---
+++ src/component/dt.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class dt extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default dt;
